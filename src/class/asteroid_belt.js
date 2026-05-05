f67bba6e3d909f7be576e3f8888774112f1c6344
AlexisGerv <gervaudalexis@gmail.com>
2026-05-05 19:06:55 +0200

    correction des ombres des astéroides

--- src/class/asteroid_belt.js
+++ src/class/asteroid_belt.js
@@ -46,7 +46,7 @@ export default class CeintureAsteroides {
 
     this.mesh = new THREE.InstancedMesh(geometry, material, this.nombre);
     this.mesh.castShadow = false;     // 1500 ombres tueraient les FPS
-    this.mesh.receiveShadow = true;
+    this.mesh.receiveShadow = false;
 
     // Objet temporaire pour composer chaque matrice d'instance.
     const dummy = new THREE.Object3D();
